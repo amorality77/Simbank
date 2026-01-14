8fbc65dca923d9bf9edcd882140d78ace42dc41f
11th - Amorality <11th-amorality@MacBook-Air-11th.local>
2026-01-14 09:37:07 +0600

    Registration v2

--- src/kg/sim_bank/registration/Registration.java
+++ src/kg/sim_bank/registration/Registration.java
@@ -4,6 +4,7 @@ public class Registration {
     public static void main(String[] args) {
 
         System.out.println("Registration... ");
+        System.out.println("Registration...v2 ");
 
     }
 }
